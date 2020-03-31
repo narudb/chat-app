632a69d209d010cdcb31cdec1a9b70412a3014e6
narudb <benjam.d@gmail.com>
2020-03-31 10:51:48 +0200

    modifs q02.2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+        name = "Tracey Kim"
+        avatar = "https://randomuser.me/api/portraits/women/90.jpg"
+        online
+      />
+      <Contact 
+        name = "Judd Gibson"
+        avatar = "https://randomuser.me/api/portraits/men/57.jpg"
+      />
+      <Contact 
+        name = "Jackson Johnston"
+        avatar = "https://randomuser.me/api/portraits/men/40.jpg"
+        online
+      />
     </div>
   );
 }
 
 export default App;
+
+
